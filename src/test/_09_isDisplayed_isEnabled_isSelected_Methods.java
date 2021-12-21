d6154c7dc752b5ab5bae68f42b462676599927d0
akinkayagithub <akinkaya.b@gmail.com>
2021-12-21 16:39:17 -0600

    added selenium basic methods

---
+++ src/test/_09_isDisplayed_isEnabled_isSelected_Methods.java
@@ -0,0 +1,41 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import utils.Driver;
+
+public class _09_isDisplayed_isEnabled_isSelected_Methods {
+    /*
+    -All these methods are used with web elements and return true or false
+    -isDisplayed() method is used to verify the presence of a particular web element and, it will return true if element is found, and false otherwise
+    -isEnabled() is mostly used with buttons, and it verifies if the web element is enabled or disabled
+    -isSelected() is mostly used with radio buttons, dropdowns and checkboxes, and it verifies if the web element is selected or not
+
+    TEST CASE:
+    1. Go to "https://www.united.com/en/us"
+    2. Validate the "UNITED" logo is displayed
+    3. Validate the "Round trip" radio button is selected by default
+    3. Validate the "Find flights" button is enabled by default
+     */
+
+    public static void main(String[] args) {
+        // 1. Use Driver util method - getDriver() to set driver to be used for the script
+        WebDriver driver = Driver.getDriver();
+
+        // 2. Navigate to "https://www.united.com/en/us" using get() method
+        driver.get("https://www.united.com/en/us");
+
+        // 3. Locate the "Round trip" radio button and "Find flights" button elements and validate them
+        WebElement unitedLogo = driver.findElement(By.id("unitedLogo"));
+        WebElement roundTripRadioButton = driver.findElement(By.id("roundtrip"));
+        WebElement findFlightsButton = driver.findElement(By.cssSelector("button[aria-label='Find flights']"));
+
+        System.out.println(unitedLogo.isDisplayed()); // true
+        System.out.println(roundTripRadioButton.isSelected()); // true
+        System.out.println(findFlightsButton.isEnabled()); // true
+
+        // 4. Use Driver util method - quitDriver() to quit browser session
+        Driver.quitDriver();
+    }
+}
---
+++ src/test/_10_findElement_findElements_Methods.java
@@ -0,0 +1,42 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import utils.Driver;
+
+import java.util.List;
+
+public class _10_findElement_findElements_Methods {
+    /*
+    -There are mainly 2 methods helps us to locate elements
+    -findElement() method returns a single web element, and it can be stored in a WebElement variable
+    -findElements() method returns a list of web elements, and they can be stored in a List<WebElement>
+
+    NOTE: if element is not found with findElement() method, it will throw a NoSuchElementException
+    NOTE: if elements are not found with findElements() method, it will not throw an exception and returns size as zero
+
+    TEST CASE:
+    1. Go to https://www.wikipedia.org/
+    2. Validate the first primary language link is "English ..."
+    3. Validate there are 10 primary languages displayed around the wiki image logo
+     */
+
+    public static void main(String[] args) {
+        // 1. Use Driver util method - getDriver() to set driver to be used for the script
+        WebDriver driver = Driver.getDriver();
+
+        // 2. Navigate to "https://www.wikipedia.org/" using get() method
+        driver.get("https://www.wikipedia.org/");
+
+        // 3. Locate the primary languages and validate them
+        WebElement wikiFirstPrimaryLanguage = driver.findElement(By.cssSelector("div[class='central-featured'] a"));
+        List<WebElement> listOfPrimaryLanguages= driver.findElements(By.cssSelector("div[class='central-featured'] a"));
+
+        System.out.println(wikiFirstPrimaryLanguage.getText()); // English ...
+        System.out.println(listOfPrimaryLanguages.size()); // 10
+
+        // 4. Use Driver util method - quitDriver() to quit browser session
+        Driver.quitDriver();
+    }
+}
